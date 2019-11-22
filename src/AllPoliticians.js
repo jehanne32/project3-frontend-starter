aafca736e45ccf33221609a98f2662ab6632dfd4
cthomas502 <coreythomas14@gmail.com>
2019-11-22 09:35:14 -0500

    Polit working

--- src/AllPoliticians.js
+++ src/AllPoliticians.js
@@ -1,30 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+const databaseUrl = process.env.HEROKU_DB_URL || 'http://localhost:3000'
 
 class AllPoliticians extends React.Component {
     state = {
-        politicians: []
-      }
-    
-      componentDidMount() {
-        this.getPoliticians()
-      }
-    
-      getPoliticians = () => {
-        axios({
-          url: `${databaseUrl}/politicians`,
-          method: 'get'
-        })
-          .then(response => {
-            console.log(response)
-            // this.setState({ politicians })
-          })
-      }
-      
-    render() {
-        return (
-
-        )
+        candidates: []
     }
+
+    componentDidMount() {
+        this.getCandidates()
+    }
+
+    getCandidates = () => {
+        axios({
+            url: `${databaseUrl}/candidates`,
+            method: 'get'
+        })
+            .then(response => {
+                this.setState({ candidates: response.data.candidates })
+            })
+    }
+
+render() {
+    console.log(this.state.candidates)
+    let tempCandid = this.state.candidates.map(candidate => {
+        console.log(candidate.name)
+        return <h3 key={candidate.id}>{candidate.name}</h3>
+        //   <div>
+        //       <img className="CandidateImages" src="../public/images/blabla" alt=""><a href=""/></img>
+        //   </div>
+    })
+
+    return (
+        <div>
+            {tempCandid}
+        </div>
+    )
+}
 }
 
 export default AllPoliticians
--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import './App.css';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link
-// } from 'react-router-dom';
-import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import AllPoliticians from './AllPoliticians';
-import OnePolitician from './OnePolitician';
+// import OnePolitician from './OnePolitician';
 // import Home from './Home.js';
-// import AllPoliticians from './AllPoliticians.js';
-// import OnePolitician from './OnePolitician.js';
-const databaseUrl = process.env.HEROKU_DB_URL || 'http://localhost:3000'
+
 
 class App extends React.Component {
 
   render() {
-    console.log(this.state)
     console.log("Rendered")
     return (
       <Router basename='/'>
-        <nav></nav>
+        <nav>
+          <Link to="/allpoliticians">Candidates</Link>
+        </nav>
         <div className="App" >
-          {(this.state.politicians) ?
-          <div>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/allpoliticians" component={AllPoliticians}/>
-            <Route exact path="/onepolitician" component={OnePolitician}/>
-          </div> 
-        : <Route exact path="/" component={Home}/> }
+          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/allpoliticians" component={AllPoliticians} />
+          {/* <Route exact path="/onepolitician" component={OnePolitician} /> */}
         </div>
       </Router>
     );
