0985174f891f1b19d4d708c4657322232dd4793e
Michelle Jensen <mchlljnsn@yahoo.com>
2019-11-26 08:38:45 -0500

    Cleaned up image file

--- src/App.js
+++ src/App.js
@@ -23,16 +23,6 @@ class App extends React.Component {
 
             <Route exact path="/onepolitician/:candidateID" component={OnePolitician} />
           </div>
-          {/* <p className="para">Our aim is to provide a factual, nonpartisan app where 
-          voters can access information about the Republican and Democratic candidates 
-          for the 2020 United States presidential election.  Our visitors can access 
-          information about candidates recent voting records, what offices they have 
-          held, their positions on the key issues, links to the candidates websites 
-          for in-depth learning on individuals and other important issues that will 
-          enable voters to make an informed decision when choosing who to vote for.</p>
-
-          <p className="para">We have design   ed our app to be to easy to use and understand. 
-          If you have any questions about our app, please contact us.</p> */}
 
         </div>
       </Router>
